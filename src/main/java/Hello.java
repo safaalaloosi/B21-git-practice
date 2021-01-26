fa5a2820552524d5f18afd04012d5bed2915350a
safaalaloosi <salaloosii@gmail.com>
2021-01-26 10:43:31 -0800

    B21 learning git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("B21 future SDET learning GIT");
 
         //testing my hello
     }
